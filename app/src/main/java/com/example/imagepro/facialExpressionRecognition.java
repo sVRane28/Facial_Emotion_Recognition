9f0635935219667d2bce3d999ffa27bf121c6692
Smrithi <smrithirane.28@gmail.com>
2021-06-27 15:26:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagepro/facialExpressionRecognition.java
@@ -0,0 +1,73 @@
+package com.example.imagepro;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import org.opencv.objdetect.CascadeClassifier;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.gpu.GpuDelegate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class facialExpressionRecognition {
+    private Interpreter interpreter;
+    private int INPUT_SIZE;
+    private int height = 0;
+    private int width = 0;
+    private GpuDelegate gpuDelegate = null;
+    private CascadeClassifier cascadeClassifier;
+
+    facialExpressionRecognition(AssetManager assetManager, Context context, String modelPath, int inputSize) throws IOException {
+        INPUT_SIZE = inputSize;
+        Interpreter.Options options = new Interpreter.Options();
+        gpuDelegate = new GpuDelegate();
+        options.addDelegate(gpuDelegate);
+        options.setNumThreads(4);
+        interpreter = new Interpreter(loadModelFile(assetManager,modelPath),options);
+        Log.d( tag:"facial_Expression",msg:"Model is loaded");
+
+        try{
+            InputStream is=context.getResources().openRawResource(R.raw.haarcascade_frontalface_alt);
+            File cascadeDir = context.getDir(name: "cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, child: "haarcascade_frontalface_alt");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+            byte[] buffer = new byte[4096];
+            int byteRead;
+            while((byteRead=is.read(buffer)) !=1){
+                os.write(buffer, off: 0,byteRead);
+            }
+
+            is.close();
+            os.close();
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            Log.d(tag: "facial_expression", msg: "Classifier is loaded");
+
+
+
+        }
+        catch(IOException e){
+        e.printStackTrace();
+        }
+    }
+
+    private MappedByteBuffer loadModelFile(AssetManager assetManager, String modelPath) throws IOException{
+        AssetFileDescriptor assetFileDescriptor = assetManager.openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(assetFileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+
+        long startOffset = assetFileDescriptor.getStartOffset();
+        long declaredLength = assetFileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset,declaredLength);
+
+    }
+
+
+}
